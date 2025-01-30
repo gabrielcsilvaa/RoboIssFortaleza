a2044fb83e3cfd5dfc3635f0f20fa010b9056296
Gabriel <gabrielcardososilva290@gmail.com>
2025-01-30 09:05:27 -0300

    Gabriel ATUALIZANDO ERROS Q USUARIO DESCOBRIU

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+import re
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-import os 
 from get_year_month import get_previos_month_and_year
 from login import authUser
 from inscricao import click_inscricao
 from post_data import escriturarData, escriturando1,finishInscricao, escrituracaoFinalStretch
 
 cnpj = input('Digite o CNPJ da empresa: ')
+mes = input('informe o mês desejado(exemplo 5):  ')
+ano = input('informe o ano desejado(exemplo 2024): ')
+ano = str(ano)
+mes = int(mes)
+
 
 nome_planilha = 'planilha_robo_iss.xlsx'
 caminho_planilha = './planilha_robo_iss.xlsx'
@@ -20,9 +25,9 @@ def configurar_driver():
     driver = webdriver.Chrome(service=service, options=options)
     return driver
 
-mesANO = get_previos_month_and_year()
-mes = mesANO[0]
-ano = str(mesANO[1])
+# mesANO = get_previos_month_and_year()
+# mes = mesANO[0]
+# ano = str(mesANO[1])
 
 
 
